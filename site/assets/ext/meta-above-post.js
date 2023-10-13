eaa1698798ef6d1bb8b01ed60ab9cefb5ee109f8
Philipp Kopp <philipp.kopp@netlight.com>
2023-10-14 00:09:39 +0200

    build

---
+++ site/assets/ext/meta-above-post.js
@@ -0,0 +1,15 @@
+var meta = document.querySelector(".md-post__meta")
+meta.style["list-style"] = "none"
+meta.style["margin-left"] = "0px"
+
+var list = meta.querySelector(".md-nav__list")
+list.style["display"] = "flex"
+list.style["list-style"] = "none"
+list.style["margin-left"] = "-20px"
+Array.prototype.forEach.call(list.children, child => {
+    child.style["margin-left"] = "8px"
+    child.querySelector("div").style["gap"] = "0px"
+});
+meta.querySelector(".md-post__title").remove()
+var content = document.querySelector(".md-content__inner")
+content.insertBefore(meta, content.childNodes[3])
