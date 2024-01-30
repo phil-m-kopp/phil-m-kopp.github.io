1efe763ccd841c3f35214cf9156b2dbe2c76f860
Philipp Kopp <philipp.kopp@netlight.com>
2024-01-30 18:00:07 +0100

    Convert only if webp not existing

--- convertWebP.py
+++ convertWebP.py
@@ -12,6 +12,11 @@ def convert_to_webp(source):
     """
     destination = source.with_suffix(".webp")
 
+    # Check if the .webp file already exists
+    if destination.exists():
+        # print(f"Skipped: {source} already has a .webp version.")
+        return None
+
     image = Image.open(source)  # Open image
     # image.thumbnail((512,512), Image.Resampling.LANCZOS)
     image.save(destination, format="webp", method=6)  # Convert image to webp
@@ -24,10 +29,12 @@ def main():
     files_grabbed = []
     for files in types:
         files_grabbed.extend(Path("docs/articles").glob(files))
+        files_grabbed.extend(Path("docs/assets").glob(files))
 
     for path in files_grabbed:
         webp_path = convert_to_webp(path)
-        print(webp_path)
+        if webp_path:
+            print(webp_path)
     print("---done converting to webp---")
 
 main()
